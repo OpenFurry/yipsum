1078c9b894a224a1e25f42194692bb1a746f9680
Matthew Scott <matthew.scott@canonical.com>
2014-07-09 17:48:55 -0600

    Initial creation

---
+++ yipsum.js
@@ -0,0 +1,98 @@
+var sourceWords = [
+  // Variations on fur
+  'fur',
+  'furry',
+  'furre',
+  'furries',
+
+  // Conventions
+  'con',
+  'convention',
+  'anthrocon',
+  'AC',
+  'Further Confusion',
+  'FC',
+  'Midwest Fur Fest',
+  'MFF',
+
+  // Fursuiting
+  'fursuit',
+  'suiter',
+  'headless',
+  'ruining the magic',
+
+  // Various
+  'popufur',
+  'feesh',
+
+  // Art
+  'commission',
+  'stream',
+  'sketch',
+  'sketch stream',
+  'icon',
+
+  // Chat
+  'furnet',
+  'anthrochat',
+  'FurryMUCK',
+  'SPR',
+  'taps',
+  'tapestries',
+  'furcadia',
+
+  // Websites
+  'furnation',
+  'Yerf!',
+  'VCL',
+  'FA',
+  'FurAffinity',
+  'SoFurry',
+  'Weasyl',
+  'InkBunny',
+  'Wikifur',
+
+  // Species
+  'sergal',
+  'fox',
+  'wolf',
+  'husky',
+  'cat',
+
+  // Noises
+  'yap',
+  'yip',
+  'bark',
+  'mew',
+  'meow',
+  'growl',
+  'murr',
+
+  // Verbs
+  'wiggle',
+  'scritch',
+  'pet',
+];
+
+$(document).ready(function() {
+  result = '';
+  _(5).times(function(paragraph) {
+    result += '<p>';
+    _(7).times(function(sentence) {
+      _(Math.floor((Math.random() * 10) / 3) + 1).times(function(phrase) {
+        _(Math.floor(Math.random() * 10)).times(function(word) {
+          sourceWord = _.sample(sourceWords);
+          if (phrase === 0 && word === 0) {
+            sourceWord = sourceWord.charAt(0).toUpperCase() + sourceWord.slice(1);
+          }
+          result += sourceWord + ' ';
+        });
+        result = result.slice(0, result.length - 1) + ', ';
+      });
+      result = result.slice(0, result.length - 2) + '.  ';
+    });
+    result += '</p>';
+  });
+
+  $('#yipsum').html(result);
+});
